f08d28c2975fd0a41b5d6cd58edafe15db505f3a
Elijah Conners <business@elijahpepe.com>
2022-08-08 12:21:12 -0700

    chore: tweak design of homepage hero title
    Signed-off-by: Elijah Conners <business@elijahpepe.com>

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,12 +1,19 @@
+import presetWebFonts from '@unocss/preset-web-fonts';
 import { defineConfig, presetUno } from 'unocss';
 
 export default defineConfig({
 	theme: {
 		colors: {
-			'multiversusGray': '#141629',
+			multiversusGray: '#141629',
 		},
 	},
 	presets: [
 		presetUno({ dark: 'class' }),
+		presetWebFonts({
+			provider: 'google',
+			fonts: {
+				bolded: ['Barlow Semi Condensed:bolditalic'],
+			},
+		}),
 	],
 });
